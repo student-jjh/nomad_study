8fc697fe1a6b0de22006cd8ba1ae4fd2d48eb9c1
JJH <skkuwjswnsgur456@gmail.com>
2021-07-21 18:47:22 +0900

    end to 2.6 Bollean

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-alert("hi");
+const a = 5;
+const b = 2;
+
+let myName = 'JJH';
+
+console.log(myName);
+console.log(a+b);
+console.log(a * b);
+console.log(a/b);
+myName="KHY"
+console.log("your name is " + myName)
+
+const amIFat = true;
+console.log(amIFat);
+let something;
+console.log(something)
