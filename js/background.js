c12efc696f6f5aaad003fc290bd0be5d51bed126
JJH <skkuwjswnsgur456@gmail.com>
2021-08-05 23:17:57 +0900

    background

--- js/background.js
+++ js/background.js
@@ -1,11 +1,14 @@
 const images = ['img1.jpg','img2.jpg','img3.jpg','img4.jpg','img5.jpg','img6.jpg','img7.jpg','img8.jpg','img9.jpg'];
 
 const chosenImage = images[Math.floor((Math.random()*images.length))];
-
-const bgImage = document.createElement("img");
+//const background=document.getElementById("background")
+//const bgImage = document.createElement("img");
 const body = document.body;
-bgImage.id='image';
-bgImage.src = `img/${chosenImage}`
- 
-
-document.body.appendChild(bgImage);
+//bgImage.id='image';
+//bgImage.src = `img/${chosenImage}`
+//console.log(bgImage.src);
+body.style.backgroundImage=`url(img/${chosenImage})`;
+body.style.backgroundRepeat='no-repeat';
+body.style.backgroundSize='100% 100%';
+body.style.height='100%';
+//document.body.appendChild(bgImage);
